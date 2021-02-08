3c3098fdeb2c2a3581225e56aa8664dce33f5d7f
morten <mortenk@live.com>
2021-02-08 16:37:53 +0100

    Added failing test to MapTest

--- src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
+++ src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
@@ -1,4 +1,19 @@
 package inf112.skeleton.app.mechanics.map;
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import inf112.skeleton.app.HelloWorld;
+import inf112.skeleton.app.screens.MainMenuScreen;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
 public class MapTest {
+
+    @Test
+    public void testMapObject() {
+        Map map = new Map("example.tmx");
+
+        assertNull(map);
+    }
 }
