025c7a217ccdc60a30f609ced9c8e79674fda49d
morten <mortenk@live.com>
2021-02-09 10:31:52 +0100

    Passing test of Map.create(), but it needs manual closing of the window

--- src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
+++ src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
@@ -2,18 +2,32 @@ package inf112.skeleton.app.mechanics.map;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import inf112.skeleton.app.HelloWorld;
-import inf112.skeleton.app.screens.MainMenuScreen;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class MapTest {
+    Map map;
+    Lwjgl3Application app;
+    Lwjgl3ApplicationConfiguration cfg;
+
+    @Before
+    public void setup() {
+        cfg = new Lwjgl3ApplicationConfiguration();
+        cfg.setTitle("RoboRally");
+//        cfg.setInitialVisible(false);
+        cfg.setWindowedMode(500, 500);
+
+        map = new Map("example.tmx");
+        app = new Lwjgl3Application(map, cfg);
+        app.exit();
+
+    }
 
     @Test
     public void testMapObject() {
-        Map map = new Map("example.tmx");
-
-        assertNull(map);
+        assertNotNull(map.tiledMap);
     }
 }
