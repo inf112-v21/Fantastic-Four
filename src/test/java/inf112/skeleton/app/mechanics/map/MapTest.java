8dd199a8fb24bec0ddc66b0b74a734eb012b406b
morten <mortenk@live.com>
2021-02-09 10:42:44 +0100

    Passing test for Map.create() storing layers

--- src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
+++ src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
@@ -3,18 +3,19 @@ package inf112.skeleton.app.mechanics.map;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class MapTest {
-    Map map;
-    Lwjgl3Application app;
-    Lwjgl3ApplicationConfiguration cfg;
+    static Map map;
+    static Lwjgl3Application app;
+    static Lwjgl3ApplicationConfiguration cfg;
 
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("RoboRally");
 //        cfg.setInitialVisible(false);
@@ -29,5 +30,13 @@ public class MapTest {
     @Test
     public void testMapObject() {
         assertNotNull(map.tiledMap);
+    }
+
+    @Test
+    public void testLayersAreCreated() {
+        assertNotNull(map.boardLayer);
+        assertNotNull(map.playerLayer);
+        assertNotNull(map.holeLayer);
+        assertNotNull(map.flagLayer);
     }
 }
