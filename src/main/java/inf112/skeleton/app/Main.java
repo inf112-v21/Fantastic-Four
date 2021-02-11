dd74679031183335e49a5fb72dc9d1ff4616f913
morten <mortenk@live.com>
2021-02-11 12:15:04 +0100

    Main starts an example game

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -2,6 +2,7 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import inf112.skeleton.app.mechanics.map.Map;
 import inf112.skeleton.app.screens.MainMenuScreen;
 
 public class Main {
@@ -9,9 +10,9 @@ public class Main {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("RoboRally");
 
-        MainMenuScreen ui = new MainMenuScreen();
+        Map ui = new Map("example.tmx");
 
-        cfg.setWindowedMode(500, 700);
+        cfg.setWindowedMode(500, 500);
 
         new Lwjgl3Application(ui, cfg);
     }
