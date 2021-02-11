7c75f7fb152c610f54e8fc17606f21815dac70e2
morten <mortenk@live.com>
2021-02-11 21:10:29 +0100

    Clean up of tests

--- src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
+++ src/test/java/inf112/skeleton/app/mechanics/map/MapTest.java
@@ -2,22 +2,23 @@ package inf112.skeleton.app.mechanics.map;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class MapTest {
     static Map map;
     static Lwjgl3Application app;
     static Lwjgl3ApplicationConfiguration cfg;
     static String mapName = "example.tmx";
+
     @BeforeClass
     public static void setup() {
         cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("RoboRally");
-//        cfg.setInitialVisible(false);
+//        cfg.setInitialVisible(false); // Can be uncommented when (if) the Gdx.app.exit() call actually closes the app
         cfg.setWindowedMode(500, 500);
 
         map = new Map(mapName);
