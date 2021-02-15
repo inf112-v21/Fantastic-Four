650d743c3220bb7f379d646474520c5800ee5768
obertaas <danielsaasheim@gmail.com>
2021-02-15 20:42:10 +0100

    created class

---
+++ src/main/java/inf112/skeleton/app/assets/cards/ProgramDeck.java
@@ -0,0 +1,14 @@
+package inf112.skeleton.app.assets.cards;
+
+import java.util.Stack;
+
+public class ProgramDeck extends AbstractDeck {
+
+    //static Stack
+
+    Stack<ProgramCard> discardedCards;
+    Stack<ProgramCard> cardsInPlay;
+
+    public ProgramDeck() {
+    }
+}
