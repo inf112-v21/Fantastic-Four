a2193272412605427cfad18f747e920d9e26295c
obertaas <danielsaasheim@gmail.com>
2021-02-15 19:03:55 +0100

    created Player class

--- src/main/java/inf112/skeleton/app/assets/Player.java
+++ src/main/java/inf112/skeleton/app/assets/Player.java
@@ -1,4 +1,21 @@
 package inf112.skeleton.app.assets;
 
 public class Player {
+
+    private String playerName;
+    private ProgramSheet programSheet;
+    private Robot robot;
+
+    public Player(String playerName) {//throws InstantiationException {
+        this.playerName = playerName;
+        this.programSheet = new ProgramSheet(playerName);
+        try {
+            this.robot = new Robot();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
+
