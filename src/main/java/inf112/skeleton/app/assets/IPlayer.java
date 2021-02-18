b899ec190d6433f5b5876b5123eeea48f082474f
morten <mortenk@live.com>
2021-02-18 13:36:24 +0100

    Added getDamage() and updated javadocs

--- src/main/java/inf112/skeleton/app/assets/IPlayer.java
+++ src/main/java/inf112/skeleton/app/assets/IPlayer.java
@@ -3,5 +3,15 @@ package inf112.skeleton.app.assets;
 import inf112.skeleton.app.assets.cards.IDeck;
 
 public interface IPlayer {
-    void receive(IDeck cards); // Receive an IDeck of cards to pick from (example: Receive 9 cards to pick 5 from)
+    /**
+     * Receive an IDeck of cards to pick from (example: Receive 9 cards to pick 5 from)
+     * @param cards The cards to pick from
+     */
+    void receive(IDeck cards);
+
+    /**
+     *
+     * @return The damage this player has gotten
+     */
+    int getDamage();
 }
