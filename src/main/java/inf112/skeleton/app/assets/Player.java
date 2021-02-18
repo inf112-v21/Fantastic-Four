1f105ee8665887a40903ed44550e20197339a22c
obertaas <danielsaasheim@gmail.com>
2021-02-18 18:10:48 +0100

    updated field variables of Player class, added getters (incl. toString())

--- src/main/java/inf112/skeleton/app/assets/Player.java
+++ src/main/java/inf112/skeleton/app/assets/Player.java
@@ -1,21 +1,50 @@
 package inf112.skeleton.app.assets;
 
+import inf112.skeleton.app.assets.cards.IDeck;
+
 public class Player implements IPlayer{
 
     private String playerName;
-    private ProgramSheet programSheet;
     private Robot robot;
 
     public Player(String playerName) {//throws InstantiationException {
         this.playerName = playerName;
-        this.programSheet = new ProgramSheet(playerName);
         try {
             this.robot = new Robot();
         } catch (InstantiationException e) {
-            e.printStackTrace();
+            e.getMessage();
         }
 
     }
 
+    public Player(String playerName, String robotName) {//throws InstantiationException {
+        this.playerName = playerName;
+        try {
+            this.robot = new Robot(robotName);
+        } catch (InstantiationException e) {
+            e.getMessage();
+        }
+
+    }
+
+
+    @Override
+    public void receive(IDeck cards) {
+
+    }
+
+    @Override
+    public int getDamage() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return playerName;
+    }
+
+    public Robot getRobot() {
+        return robot;
+    }
 }
 
