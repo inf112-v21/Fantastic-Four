eb51fbf79136f104c2e5ff62485a7798e609fa5f
morten <mortenk@live.com>
2021-02-18 13:36:57 +0100

    Updated dealer logic

--- src/main/java/inf112/skeleton/app/game/Game.java
+++ src/main/java/inf112/skeleton/app/game/Game.java
@@ -11,18 +11,18 @@ import java.util.List;
 public class Game {
 
     IDeck deck;
-    IDeck discardpile;
+    IDeck discardPile;
     List<IPlayer> players;
     final int MAX_NUMBER_OF_CARDS = 9;
 
     public Game() {
         deck = new ProgramDeck();
 //        deck.create() // TODO Match with the name of Edyta's method for creating all program cards.
-        discardpile = new ProgramDeck();
+        discardPile = new ProgramDeck();
         players = new LinkedList<>();
     }
 
-    public void deal() {
+    public void dealProgramCards() {
         for (IPlayer player : players) {
             IDeck cards = new ProgramDeck(); // Create a small deck of cards for each player
 //            cards.deal(MAX_NUMBER_OF_CARDS - player.getDamage()); // TODO Match with the name of Edyta's method for creating all cards and getDamage() in Player
@@ -30,9 +30,9 @@ public class Game {
         }
     }
 
-    public void returnToDiscardpile(IDeck cards) {
-        for (ICard card : cards) { // TODO Get a stack from cards, like cards.getCards()
-            discardpile.add(card);
+    public void programCardsToToDiscardPile(IDeck cards) {
+        for (ICard card : cards.getCards()) { // TODO Get a stack from cards, like cards.getCards()
+            discardPile.add(card);
         }
     }
 }
