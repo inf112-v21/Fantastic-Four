b1adb480d483758eadeb3bd85680369b24599465
morten <mortenk@live.com>
2021-02-18 13:28:19 +0100

    Added receive() to IPlayer

--- src/main/java/inf112/skeleton/app/assets/IPlayer.java
+++ src/main/java/inf112/skeleton/app/assets/IPlayer.java
@@ -1,4 +1,7 @@
 package inf112.skeleton.app.assets;
 
+import inf112.skeleton.app.assets.cards.IDeck;
+
 public interface IPlayer {
+    void receive(IDeck cards); // Receive an IDeck of cards to pick from (example: Receive 9 cards to pick 5 from)
 }
