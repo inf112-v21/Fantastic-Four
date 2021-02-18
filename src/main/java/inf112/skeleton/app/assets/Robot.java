a605528ee7218f71fc721160b980865b13776849
obertaas <danielsaasheim@gmail.com>
2021-02-18 18:11:53 +0100

    added ProgramSheet to field variables

--- src/main/java/inf112/skeleton/app/assets/Robot.java
+++ src/main/java/inf112/skeleton/app/assets/Robot.java
@@ -7,32 +7,51 @@ import java.util.Map;
 public class Robot {
 
     private final static int NUMBER_OF_ROBOTS = 8;
-    private final static String[] robotNames = {"A", "B", "C"};
-    private static HashMap<String, Boolean> availableRobots;
+    private final static String[] robotNames = {"Twonky", "Squash Bot", "Twitch", "Zoom Bot",
+                                                "Hammer Bot", "Spin Bot", "Hulk X90", "Trundle Bot"};
+    private static HashMap<String, Boolean> availableRobots = new HashMap<>();
     static {
         for (String name : robotNames) availableRobots.put(name, true);
     }
 
     private String robotName;
+    private ProgramSheet programSheet;
+
 
     public Robot() throws InstantiationException {
-        if (!availableRobots.containsValue(true)) throw new InstantiationException();
+        if (!availableRobots.containsValue(true)) throw new InstantiationException("No robots are available...");
         for (String robotName : availableRobots.keySet()) {
             if (availableRobots.get(robotName)) {
                 this.robotName = robotName;
+                setRobotToUnavailable(robotName);
+                this.programSheet = new ProgramSheet();
                 break;
             }
         }
-        updateAvailableRobots(robotName);
 
     }
 
-    private void updateAvailableRobots(String robotName) {
+    public Robot(String robotName) throws InstantiationException {
+        if (!availableRobots.get(robotName)) throw new InstantiationException("The selected robot is unavailable...");
+        this.robotName = robotName;
+        setRobotToUnavailable(robotName);
+        this.programSheet = new ProgramSheet();
+    }
+
+
+    private void setRobotToUnavailable(String robotName) {
         availableRobots.put(robotName, false);
     }
 
 
+    public ProgramSheet getProgramSheet() {
+        return this.programSheet;
+    }
 
+    @Override
+    public String toString() {
+        return this.robotName;
+    }
 
 
 }
