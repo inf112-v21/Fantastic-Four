e5cad27d5a356ccaf9ac1b4f13422ead5756b56a
morten <mortenk@live.com>
2021-02-18 13:38:23 +0100

    Updated IDeck with javadoc and getCards()

--- src/main/java/inf112/skeleton/app/assets/cards/IDeck.java
+++ src/main/java/inf112/skeleton/app/assets/cards/IDeck.java
@@ -1,5 +1,15 @@
 package inf112.skeleton.app.assets.cards;
 
 public interface IDeck {
-    void add(ICard card); // Add a single card to this deck
+    /**
+     * Add a single card to this deck
+     * @param card The card to be added
+     */
+    void add(ICard card);
+
+    /**
+     *
+     * @return An iterable version of this deck
+     */
+    Iterable<? extends ICard> getCards();
 }
