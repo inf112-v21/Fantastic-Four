35c895ddfc1bd028c33186d06ca11fd1e47206bd
obertaas <danielsaasheim@gmail.com>
2021-03-01 15:00:42 +0100

    added ´getProgramCard()´ method

--- src/main/java/inf112/skeleton/app/assets/IProgramSheet.java
+++ src/main/java/inf112/skeleton/app/assets/IProgramSheet.java
@@ -1,7 +1,9 @@
 package inf112.skeleton.app.assets;
 
+import inf112.skeleton.app.assets.cards.ProgramCard;
+
 public interface IProgramSheet {
 
 
-
+    ProgramCard getProgramCard(int registerNumber);
 }
