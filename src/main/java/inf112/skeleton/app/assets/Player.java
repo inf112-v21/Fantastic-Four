2249e7918a44a54ef7f94ea554f21ba98ebbc89c
obertaas <danielsaasheim@gmail.com>
2021-03-01 14:59:58 +0100

    added move methods

--- src/main/java/inf112/skeleton/app/assets/Player.java
+++ src/main/java/inf112/skeleton/app/assets/Player.java
@@ -1,23 +1,28 @@
 package inf112.skeleton.app.assets;
 
+import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.assets.cards.IDeck;
+import inf112.skeleton.app.assets.cards.ProgramCard;
+import inf112.skeleton.app.assets.cards.ProgramDeck;
 
 public class Player implements IPlayer{
 
     private String playerName;
     private Robot robot;
+    private ProgramDeck receivedProgramCards;
 
-    public Player(String playerName) {//throws InstantiationException {
+
+
+    public Player(String playerName) {
         this.playerName = playerName;
         try {
             this.robot = new Robot();
         } catch (InstantiationException e) {
             e.getMessage();
         }
-
     }
 
-    public Player(String playerName, String robotName) {//throws InstantiationException {
+    public Player(String playerName, String robotName) {
         this.playerName = playerName;
         try {
             this.robot = new Robot(robotName);
@@ -26,12 +31,19 @@ public class Player implements IPlayer{
         }
 
     }
+    
+
+    private void registerSelectedCards(ProgramDeck cards) {
+        getRobot().getProgramSheet().registerCards(cards);
+    }
 
 
     @Override
     public void receive(IDeck cards) {
-
+        this.receivedProgramCards = (ProgramDeck) cards;
     }
+
+
 
     @Override
     public int getDamage() {
@@ -46,5 +58,25 @@ public class Player implements IPlayer{
     public Robot getRobot() {
         return robot;
     }
+
+
+
+    public ProgramCard revealProgramCard(int registerNumber) {
+        return getProgramCard(registerNumber);
+    }
+
+    public ProgramCard getProgramCard(int registerNumber) {
+        return getRobot().getProgramSheet().getProgramCard(registerNumber);
+    }
+
+    public void moveRobotByProgramCard(ProgramCard programCard) {
+        getRobot().moveByProgramCard(programCard);
+    }
+
+    public void pushRobotToPosition(Vector2 position) {
+        getRobot().pushToPosition(position);
+    }
+
+
 }
 
