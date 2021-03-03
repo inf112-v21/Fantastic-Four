de251693013f319692e2ace67cbb382adfa3f477
mvabo <trioward@gmail.com>
2021-03-03 01:58:54 +0100

    Added Network class as a collection of packet types

---
+++ src/main/java/inf112/skeleton/app/game/Network.java
@@ -0,0 +1,29 @@
+package inf112.skeleton.app.game;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.EndPoint;
+import inf112.skeleton.app.assets.Player;
+import inf112.skeleton.app.assets.cards.Card;
+
+public class Network {
+    static public final int port = 62210;
+
+    static public void register (EndPoint endPoint) {
+        Kryo kryo = endPoint.getKryo();
+        kryo.register(AddPlayer.class);
+        kryo.register(PlayCard.class);
+        kryo.register(MovePlayer.class);
+    }
+
+    static public class AddPlayer {
+        public Player player;
+    }
+
+    static public class PlayCard {
+        public Card card;
+    }
+
+    static public class MovePlayer {
+        public int x, y;
+    }
+}
