a4747fa48ba024a592fbb3388934f48891f47b57
morten <mortenk@live.com>
2021-03-04 14:52:33 +0100

    ProgramDeck changed to List for all other than hands, refactoring variable names

--- src/main/java/inf112/skeleton/app/assets/IPlayer.java
+++ src/main/java/inf112/skeleton/app/assets/IPlayer.java
@@ -1,23 +1,24 @@
 package inf112.skeleton.app.assets;
 
+import inf112.skeleton.app.assets.cards.ICard;
 import inf112.skeleton.app.assets.cards.IDeck;
 import inf112.skeleton.app.assets.cards.ProgramCard;
 import inf112.skeleton.app.assets.cards.ProgramDeck;
+
+import java.util.List;
 
 public interface IPlayer {
     /**
      * Receive an IDeck of cards to pick from (example: Receive 9 cards to pick 5 from)
      * @param cards The cards to pick from
      */
-    void receive(IDeck cards);
+    void receive(List<ICard> cards);
 
     /**
      *
      * @return The damage this player has gotten
      */
     int getDamage();
-
-    void executeProgramCard(int i);
 
     String getPlayerName();
 
@@ -29,5 +30,5 @@ public interface IPlayer {
 
     void moveRobotByProgramCard(ProgramCard programCard);
 
-    ProgramDeck getReceivedProgramCards();
+    List<ICard> getReceivedProgramCards();
 }
