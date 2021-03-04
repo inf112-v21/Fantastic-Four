82fbab46020e2543b4e491c056935a2b2e1544cd
morten <mortenk@live.com>
2021-03-04 13:43:34 +0100

    Removed duplicate code, CardDeck ready to be removed when the test is updated to ProgramDeck

--- src/main/java/inf112/skeleton/app/assets/cards/CardDeck.java
+++ src/main/java/inf112/skeleton/app/assets/cards/CardDeck.java
@@ -1,8 +1,20 @@
 package inf112.skeleton.app.assets.cards;
 
+import java.util.List;
+
 public class CardDeck extends AbstractDeck{
 
     public CardDeck() {
     	super();
+    }
+
+    @Override
+    public List<ICard> createDeck() {
+        return null;
+    }
+
+    @Override
+    public List<ICard> draw(int quantity) {
+        return null;
     }
 }
