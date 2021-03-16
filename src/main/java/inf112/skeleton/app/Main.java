8c37c17c2f268da0e19b9c5cf05f9e8a4e509926
morten <mortenk@live.com>
2021-03-16 12:28:45 +0100

    Changed window size

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -1,5 +1,6 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import inf112.skeleton.app.game.RoboGame;
@@ -8,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("RoboRally");
+        cfg.setWindowedMode(1300, 768);
 
         //Map ui = new Map("example.tmx");
         //MainMenuScreen mainMenuScreen = new MainMenuScreen();
