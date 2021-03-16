a252afb2bb21c1f328ede6314b2fe3b11d125548
morten <mortenk@live.com>
2021-03-16 13:22:49 +0100

    Changed movement in Player

--- src/main/java/inf112/skeleton/app/assets/Player.java
+++ src/main/java/inf112/skeleton/app/assets/Player.java
@@ -1,5 +1,6 @@
 package inf112.skeleton.app.assets;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.assets.cards.ICard;
 import inf112.skeleton.app.assets.cards.ProgramCard;
@@ -8,6 +9,7 @@ import java.util.List;
 
 public class Player {
 
+    private static final long TIMEBETWEENMOVES = 1000;
     private String playerName;
     private List<ProgramCard> programCards;
     private int damage, life;
@@ -21,13 +23,13 @@ public class Player {
     public static final int MIN_NUMBER_OF_DAMAGE_TOKENS = 0;
     public static final int MAX_NUMBER_OF_DAMAGE_TOKENS = 9;
 
+    long lastMove;
+
     public Player(String playerName) {
         this.playerName = playerName;
-        position = new Vector2(0, 0);
-    }
-
-    public Player(String playerName, String robotName) {
-        this.playerName = playerName;
+        direction = Definitions.Direction.NORTH;
+        lastMove = System.currentTimeMillis();
+        position = new Vector2(1, 1);
     }
 
     private void registerSelectedCards(List<ICard> cards) {
@@ -89,8 +91,36 @@ public class Player {
     }
 
     public void moveRobotByProgramCard(ProgramCard programCard) {
+        System.out.println(programCard);
+        int[] moveAndRotate = ProgramCard.interpretType(programCard.getProgramCardType());
+        position.setAngle(position.angle() + moveAndRotate[1]);
 
+        for (int step = 0; step < moveAndRotate[0]; step++) moveOneStep();
     }
+
+    public void moveOneStep() {
+        while (lastMove + TIMEBETWEENMOVES > System.currentTimeMillis()) {
+            // spin waiter TODO improve
+        }
+        lastMove = System.currentTimeMillis();
+        if (direction == Definitions.Direction.NORTH) {
+            position.set(position.x, position.y + 1);
+        }
+        else if (direction == Definitions.Direction.WEST) {
+            position.set(position.x - 1, position.y);
+        }
+        else if (direction == Definitions.Direction.SOUTH) {
+            position.set(position.x, position.y - 1);
+        }
+        else {
+            position.set(position.x + 1, position.y);
+        }
+    }
+
+//    private void move(int dx, int dy) {
+//        playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, null);
+//        playerPosition.set(playerPosition.x + dx, playerPosition.y + dy);
+//    }
 
     public List<ProgramCard> getProgramCards() {
         return programCards;
