f631ba1095b00c88aa945583ec98cddd19c9399b
morten <mortenk@live.com>
2021-03-18 13:53:30 +0100

    Added more enums

--- src/main/java/inf112/skeleton/app/assets/Definitions.java
+++ src/main/java/inf112/skeleton/app/assets/Definitions.java
@@ -10,18 +10,45 @@ public class Definitions {
     }
 
     public enum ActivityType {
-        OPENMENU,
-        WAITFORMENUSELECTION,
-        CHECKMULTIPLAYER,
-        WAITFORCONNECTIONS,
-        PICKBOARD,
-        DEALCARDS,
-        PICKCARDS,
-        EXECUTEPROGRAMCARDS1,
-        EXECUTEPROGRAMCARDS2,
-        EXECUTEPROGRAMCARDS3,
-        EXECUTEPROGRAMCARDS4,
-        EXECUTEPROGRAMCARDS5,
+        OPEN_MENU,
+        WAIT_FOR_MENU_SELECTION,
+        CHECK_MULTIPLAYER,
+        WAIT_FOR_CONNECTIONS,
+        PICK_BOARD,
+        DEAL_CARDS,
+        PICK_CARDS,
+        EXECUTE_PROGRAMCARDS_1,
+        EXECUTE_PROGRAMCARDS_2,
+        EXECUTE_PROGRAMCARDS_3,
+        EXECUTE_PROGRAMCARDS_4,
+        EXECUTE_PROGRAMCARDS_5,
         HALT
+    }
+
+    public enum Setup {
+        CHOOSE_COURSE, // denne heter PICK_BOARD over
+        CHOOSE_ROBOT,
+        DETERMINE_FIRST
+    }
+
+    public enum HowToPlay {
+        DEAL_CARDS,
+        ARRANGE_CARDS,
+        ANNOUNCE_INTENT,
+        COMPLETE_REGISTERS,
+        CLEANUP
+    }
+
+    public enum CompleteRegisters {
+        REVEAL_PROGRAMCARDS,
+        ROBOTS_MOVE,
+        BOARD_ELEMENTS_MOVE,
+        LASERS_FIRE,
+        TOUCH_CHECK_POINT
+    }
+
+    public enum CleanUp {
+        SINGLE_WRENCH,
+        WIPE_CLEAN_REGISTERS
     }
 }
