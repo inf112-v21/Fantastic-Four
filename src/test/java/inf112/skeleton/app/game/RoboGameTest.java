951153abd0480289f4460ceb91bc6331cc07316e
morten <mortenk@live.com>
2021-04-21 10:19:59 +0200

    Test of robotsMove

--- src/test/java/inf112/skeleton/app/game/RoboGameTest.java
+++ src/test/java/inf112/skeleton/app/game/RoboGameTest.java
@@ -2,9 +2,13 @@ package inf112.skeleton.app.game;
 
 import inf112.skeleton.app.assets.Definitions;
 import inf112.skeleton.app.assets.Player;
+import inf112.skeleton.app.assets.cards.ProgramCard;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoboGameTest {
     RoboGame roboGame;
@@ -16,6 +20,22 @@ public class RoboGameTest {
         player = new Player("TestPlayer");
         player.setRobotPosition(2, 2);
         roboGame.addPlayer(player);
+    }
+
+    @Test
+    public void testRobotsMove() {
+        List<ProgramCard> singleCard = new ArrayList<>();
+        singleCard.add(new ProgramCard(ProgramCard.ProgramCardType.MOVE1, 1));
+        player.receiveChosenProgramCards(singleCard);
+
+        int x = player.x;
+        int y = player.y;
+
+        roboGame.robotsMove();
+
+        // The default direction is upwards on the screen, therefore y shall change and x shall not for any MOVE card
+        Assert.assertNotEquals(y, player.y);
+        Assert.assertEquals(x, player.x);
     }
 
     @Test
